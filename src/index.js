e4222467e60f1dd33c1e9b27f2176cebba8addfd
zeeshanshaanu <zeeshanzahoor409@gmail.com>
2025-04-17 23:13:30 +0500

    add Url

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import store from './store/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./store/store";
+import { Provider } from "react-redux";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000/api/";
-// axios.defaults.baseURL = "https://saiy-admin-backend.netlify.app/";
+// axios.defaults.baseURL = "http://localhost:8000/api/";
+axios.defaults.baseURL =
+  "https://evening-atoll-98146-c8adcfc9a19d.herokuapp.com/api/";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
